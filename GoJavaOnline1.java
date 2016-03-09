aa811b8b06e926fa0debb2968049b46a3adf28da
OlegVlasenko <vlasenko.inbox@gmail.com>
2016-03-10 00:31:59 +0200

    1

---
+++ GoJavaOnline1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 28.02.2016.
+ */
+public class GoJavaOnline1 {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+
+    }
+}
